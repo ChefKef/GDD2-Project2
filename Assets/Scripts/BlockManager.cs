fd0767fbda49e63016364c65a6493b6b78b19614
sj3603 <56056374+sj3603@users.noreply.github.com>
2020-10-07 11:59:53 -0400

    Created block stuff
    Created basic block placement functionality and grid display system (in debug)

---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    public Camera gameCam;
+
+    public GameObject TestBlockPublic;
+    private GameObject testBlockPrivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Grid grid = new Grid(21, 9, 1f);
+        testBlockPrivate = TestBlockPublic;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(gameCam.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (hit.collider == null)
+            {
+                Debug.Log(hit);
+                GameObject instanceBlock;
+                instanceBlock = Instantiate(testBlockPrivate, gameCam.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+                instanceBlock.transform.position = new Vector3(Mathf.Round(instanceBlock.transform.position.x), Mathf.Round(instanceBlock.transform.position.y), 0);
+            }
+            if (hit.collider != null && hit.collider.gameObject.tag == "block")
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
