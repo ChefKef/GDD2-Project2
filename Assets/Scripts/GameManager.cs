5aad64ed1d2359cb00cb385e91e2c01e7f3756b9
Corey Rigney <coreyrigney@gmail.com>
2020-10-21 19:02:36 -0400

    Fixed title screen GameManager bug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -77,6 +77,7 @@ public class GameManager : MonoBehaviour
         failUI = GameObject.Find("FailUI");
         clearUI = GameObject.Find("ClearUI");
         levelObjects = GameObject.Find("LevelObjects");
+        groundContainer = GameObject.Find("GroundContainer");
 
         gridUI.SetActive(false);
         gameUI.SetActive(false);
@@ -146,7 +147,10 @@ public class GameManager : MonoBehaviour
         previousState = currentState;
     }
 
-    void OnDestroy() { if (this == _instance) { _instance = null; } }
+    void OnDestroy() {
+        Debug.Log("Destroyed gm");
+        if (this == _instance) { _instance = null; } 
+    }
 
     public void StartLevel(int value)
     {
